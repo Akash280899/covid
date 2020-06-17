c907d226c5d5047df579af6adf7099c0de9f8b39
Akash J <58515715+Akash280899@users.noreply.github.com>
2020-06-17 11:30:12 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,9 +12,12 @@ export class AppComponent {
   globaldata: any = {};
   countrydata: any = [];
   singledata: any = {};
+  tempdata: any = [];
+  maindata: any =[];
   common: string;
  
   assignName (commonName: string) {
+    //console.log("mainsingle" + this.singledata);
     this.common = commonName;
     console.log("value received: " + commonName);
     this.common = this.common[0].toUpperCase() + this.common.slice(1);
@@ -22,40 +27,54 @@ export class AppComponent {
     .subscribe((value:any) => {
       this.globaldata = value.Global;
       this.countrydata = value.Countries;
-      
+
+      console.log(this.countrydata);
+      if(this.common.length == 2) {
+        this.common  =this.common.toUpperCase( );
+        
+      }
+
       console.log("commonfunc" +this.common);
       this.countrydata.forEach(countrylist => {
         
         if (countrylist.CountryCode == this.common || countrylist.Country == this.common) {
           console.log("inside" + this.common);
           this.singledata = countrylist;
-          console.log(this.singledata); 
+          console.log("singledata" + this.singledata); 
          }
         else {
-          
+          //this.alertService.error("Entetr");
+          //err("enter");
         }
       });
-      
     });
   }
+
+  
+  
   
   constructor(public http: HttpClient) {
-    // this.http
-    // .get("https://api.covid19api.com/summary")
-    // .subscribe((value:any) => {
-    //   this.globaldata = value.Global;
-    //   this.countrydata = value.Countries;
-      
-    //   console.log("commoncons" +this.common);
-    //   this.countrydata.forEach(countrylist => {
-        
-    //     if (countrylist.CountryCode == "IN") {
-          
-    //       this.singledata = countrylist;
-    //     }
-    //   });
-      
-    // });
+
+    this.http
+    .get("https://api.covid19api.com/summary")
+    .subscribe((value:any) => {
+      this.globaldata = value.Global;
+      this.countrydata = value.Countries;
+
+
+      for (let i = 0;i<=93;i++) {
+        this.tempdata.push(this.countrydata[i]);
+        //console.log("tempdata" + this.tempdata);
+      }
+      for (let i = 94;i < 186;i++) {
+        this.maindata.push(this.countrydata[i]);
+        //console.log("maindata" + this.maindata);
+      }
+    });
+    
   }
+  
+  
+  
   title = 'covid';
 }
